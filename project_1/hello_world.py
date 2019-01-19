91301797579b4a2182c539bda063eec8f75b7ecf
Ubuntu <ubuntu@ip-172-31-33-34.us-west-2.compute.internal>
2019-01-19 00:37:32 +0000

    The code in the current version of "hello_world.py" was checked.

--- project_1/hello_world.py
+++ project_1/hello_world.py
@@ -1,12 +1,6 @@
-"""
-hello_world.py
-
-Simple Hello World program.
-
-ECE196 Face Recognition Project
-Author: Will Chen
-
-1. Write a Write a program that prints "Hello World!" and uses the main function convention.
-"""
-
-# TODO: Write a program that prints "Hello World!" and uses a main function.
+def main():
+	print "Hello world!"
+	return 1
+if __name__=="__main__":
+	main()
+print "Guru99"
