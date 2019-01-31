febef4f89ce002288927bd1fa8c9dc54e52357c4
Akhil <ajain@ucsd.edu>
2019-01-30 19:50:53 -0800

    I made updates.

--- project_2/process_realtime.py
+++ project_2/process_realtime.py
@@ -17,15 +17,19 @@ import time
 import cv2
 # import numpy as np
 
+face_cascade = cv2.CascadeClassifier('/usr/local/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
 
 #Adapted From Reference #2:
 #faces=face_casecade.detectMultiScale(gray, 1.3,5)
 
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (320, 240)
+camera.resolution = (120, 160)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(320, 240))
+rawCapture = PiRGBArray(camera, size=(120, 160))
+x=(120-100)/2
+y=(160-100)/2
 
 # allow the camera to warmup
 time.sleep(0.1)
@@ -35,14 +39,19 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
+     
+    #Adapted From Reference #2:
     """ 
-#Adapted From Reference #2:
-    cv.rectangle(img, (x,y),(x+w,y+h),(255,0,0),2)
     roi_gray = gray[y:y+h,x:x+w]
     cv.rectangle(image, roi_gray, )
-    """
+    """ 
     
-    image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    faces=face_cascade.detectMultiScale(image, 1.3,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(image, (x,y),(x+100,y+100),(255,255,255),2)
+        
     #cv2.resize(geisel.jpg, geisel_2.jpg, Size(), 0.5, 0.5, interpolation)	
     # show the frame
     cv2.imshow("Frame", image)
