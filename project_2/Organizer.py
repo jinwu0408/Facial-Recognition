875718de56d7c603da5554a8f2ad14c33a34028e
Ubuntu <ubuntu@ip-172-31-27-159.us-west-2.compute.internal>
2019-02-02 00:13:02 +0000

    trying to move the file

---
+++ project_2/Organizer.py
@@ -0,0 +1,16 @@
+import shutil
+import os
+
+source = "/home/ubuntu/Team2A-AJJW/images/00"
+dest1 = '/home/ubuntu/Team2A-AJJW/Project_2/Training'
+dest2 = '/home/ubuntu/Team2A-AJJW/Project_2/Validation'
+dest3 = '/home/ubuntu/Team2A-AJJW/Project_2/Testing'
+
+files = os.listdir(source)
+
+for f in files:
+    if (f.startswith("yaleB11_P00A") or f.startswith("yaleB11_P01A")):
+        shutil.move(f, dest1)
+   # elif (f.startswith("Intel") or f.startswith("intel")):
+  #      shutil.move(f, dest2)
+
