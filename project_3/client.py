a5e40b937f14f0cd8cce6e1c5c4994d1d2c9ccb5
Akhil <ajain@ucsd.edu>
2019-02-20 17:10:13 -0800

    I made updates.

--- project_3/client.py
+++ project_3/client.py
@@ -89,51 +89,6 @@ def main():
 
         # TODO: Use face detector to get faces.
 
-
-        pose = 0
-        # allow the camera to warmup
-        time.sleep(0.1)
-        counter=0;
-        found_face=False
-        # capture frames from the camera
-        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-            # grab the raw NumPy array representing the image, then initialize the timestamp
-            # and occupied/unoccupied text
-            image = frame.array
-             
-            #Adapted From Reference #2:
-            
-            image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            
-            faces=face_cascade.detectMultiScale(image, 1.3,5)
-            #for(x,y,w,h) in faces:
-            for(x,y,w,h) in faces:
-                maxWH = max([w,h])
-                #image2 = cv2.rectangle(image, (x,y),(x+maxWH,y+maxWH),(255,255,255),2)
-                found_face=True
-
-                # Crop to rectangle (x,y) -> (x+maxWH, y+maxWH)
-                croppedIm = image[y:y+maxWH, x:x+maxWH]
-                resizeIm = cv2.resize(croppedIm, (244,244))
-                cv2.imshow("Cropped", croppedIm)
-                cv2.imshow("resized",resizeIm)
-                if (counter >9) :
-                    counter = 0
-                    pose = input("Please enter the Pose number")
-                counter=counter+1
-            # show the frame
-            cv2.imshow("Frame", image)
-            # time.sleep(5)
-            key = cv2.waitKey(1) & 0xFF
-
-            # clear the stream in preparation for the next frame
-            rawCapture.truncate(0)
-
-            # if the `q` key was pressed, break from the loop
-            if key == ord("q"):
-                break
-            found_face=False
-
         # Be sure to save the faces in a variable called 'faces'
 
         for (x, y, w, h) in faces:
